4db6a17df8927aaa24f864ba825f9f6e095d0275
Kreisso <polakmacie.web@gmail.com>
2019-12-17 19:02:59 +0100

    # Added LCS # Added save button

---
+++ src/main/entity/LCS.java
@@ -0,0 +1,35 @@
+package main.entity;
+
+public class LCS {
+
+    public static final double MATCHING_PERCENT = .7;
+
+    public static boolean computeMatching(String word, String sample) {
+        StringBuilder findMatch = new StringBuilder();
+        int M = word.length();
+        int N = sample.length();
+
+        int[][] opt = new int[M + 1][N + 1];
+
+        for (int i = M - 1; i >= 0; i--) {
+            for (int j = N - 1; j >= 0; j--) {
+                if (word.charAt(i) == sample.charAt(j))
+                    opt[i][j] = opt[i + 1][j + 1] + 1;
+                else
+                    opt[i][j] = Math.max(opt[i + 1][j], opt[i][j + 1]);
+            }
+        }
+
+        int i = 0, j = 0;
+        while (i < M && j < N) {
+            if (word.charAt(i) == sample.charAt(j)) {
+                findMatch.append(word.charAt(i));
+                i++;
+                j++;
+            } else if (opt[i + 1][j] >= opt[i][j + 1]) i++;
+            else j++;
+        }
+        return findMatch.length() >= sample.length() * MATCHING_PERCENT;
+    }
+
+}
